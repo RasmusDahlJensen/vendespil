7b29acfb6613099222153ddb5db5f9a4332dea3e
Rasmus Jensen <rasmusdj96@gmail.com>
2022-10-26 12:20:44 +0200

    123

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 import cards from "./cardArray.js";
-let container = document.getElementById("gameContainer");
+
+const root = document.getElementById("root");
+root.innerHTML = `
+<div class="scoreContainer">
+<div>Score:</div>
+<div id="score">0</div>
+</div>
+<div class="flexCenter">
+<div id="gameContainer"></div>
+</div>`;
+const container = document.getElementById("gameContainer");
 
 //Here i shorten the array to 10 and then after that i doble it to fit the array
-let shortenedCards = cards.slice(0, 10);
-let doubled = shortenedCards.concat(shortenedCards);
+const shortenedCards = cards.slice(0, 10);
+const doubled = shortenedCards.concat(shortenedCards);
 
 //Doubles the cards with concat by calling it self to return a new array
 //Shuffles them with essentially the Schwartzian transform
-let cardsShuffled = doubled
+const cardsShuffled = doubled
 	.map((card) => ({ card, sort: Math.random() }))
 	.sort((a, b) => a.sort - b.sort)
 	.map(({ card }) => card);
 
 //Prints out the given values as cards on the website
-let renderGame = () => {
+const renderGame = () => {
 	cardsShuffled.forEach((card) => {
 		container.innerHTML += `
         <div class="card" id="${card.id}">
@@ -30,14 +40,25 @@ let renderGame = () => {
 renderGame();
 
 const buttons = document.querySelectorAll(".card");
+const buttonFunctionality = () => {
+	buttons.forEach((card) => {
+		card.addEventListener("click", () => {
+			playGame(event.target.parentElement.parentElement);
+		});
+	});
+};
+buttonFunctionality();
+
+//TODO: Disable eventlistener when a card is clicked so you can't match it with itself
+//Re-enable if they're not matching and flip card
 
 let cardFlips = 0;
 let cardA = 0;
 let cardB = 0;
+let score = 0;
 const playGame = (card) => {
 	//Flipped card counter
 	cardFlips++;
-	console.log(cardFlips);
 
 	if (cardFlips <= 2) {
 		card.classList.add("flip");
@@ -48,24 +69,29 @@ const playGame = (card) => {
 
 	//when two cards have been flipped:
 	if (cardFlips == 2) {
-		//If the values are matching then:
 		if (cardA == cardB) {
+			score++;
+			scoreboard.innerHTML = score;
 			const flipped = document.querySelectorAll(".flip:not(.match)");
 			flipped.forEach((flip) => flip.classList.add("match"));
 		}
 		//Reset
-		cardFlips = 0;
-		cardA = 0;
-		cardB = 0;
+		setTimeout(() => {
+			unFlip();
+		}, 750);
 	}
+	console.log(score);
 };
 
-const buttonFunctionality = () => {
-	buttons.forEach((card) => {
-		card.addEventListener("click", () => {
-			playGame(event.target.parentElement.parentElement);
-		});
+const unFlip = () => {
+	document.querySelectorAll(".card:not(.match)").forEach((card) => {
+		card.classList.remove("flip");
 	});
+	cardFlips = 0;
+	cardA = 0;
+	cardB = 0;
 };
 
-buttonFunctionality();
+//Score / timer
+
+const scoreboard = document.getElementById("score");
